b6c43f4740da6fddd00fe1932c054bb09a3245df
XinyiYS <michaelxysg@gmail.com>
2021-07-19 20:26:32 +0800

    fix a downloading bug for mnist dataset

--- utils/Data_Prepper.py
+++ utils/Data_Prepper.py
@@ -269,6 +269,12 @@ class Data_Prepper:
 
 
 from torchvision.datasets import MNIST
+MNIST.resources = [
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz', 'f68b3c2dcbeaaa9fbdd348bbdeb94873'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz', 'd53e105ee54ea40749a09fcbcd1e9432'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz', '9fb629c4189551a2d022fa330f9573f3'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz', 'ec29112dd5afa0611ce80d1b7f02629c')
+        ]
 class FastMNIST(MNIST):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)		
@@ -298,7 +304,7 @@ class FastMNIST(MNIST):
 
 		return img, target
 
-from torchvision.datasets import CIFAR10, CIFAR100
+from torchvision.datasets import CIFAR10
 class FastCIFAR10(CIFAR10):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
